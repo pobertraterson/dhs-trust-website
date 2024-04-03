ed11fca19dbaca98819feaeea620e99552e8079e
pobertraterson <pobraterson@icloud.com>
2024-04-03 14:28:57 +0100

    Added banner for all pages

---
+++ resources/src.js
@@ -0,0 +1,6 @@
+function openNavPanel() {
+    document.getElementById('sidenav').style.width = "300px";
+  }
+  function closeNavPanel() {
+    document.getElementById('sidenav').style.width = "0";
+  }
